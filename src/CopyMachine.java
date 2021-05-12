966ad09bb35295ba7feb97cfe8cd51fb14f7463c
vadim <v.melkin@haulmont.com>
2021-05-12 14:26:35 +0400

    I MADE IT

--- src/CopyMachine.java
+++ src/CopyMachine.java
@@ -2,65 +2,88 @@ import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CopyMachine {
 
-    public static Object deepCopy(Object object) throws IntrospectionException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public Object deepCopy(Object parentObject) throws IntrospectionException,
+            InvocationTargetException, InstantiationException, IllegalAccessException {
 
-        Class<?> aClass = object.getClass();
+        Class<?> aClass = parentObject.getClass(); //retrieving class of parent object
 
-        List<Field> declaredFields = Arrays.asList(aClass.getDeclaredFields());
-        List<? extends Class<? extends Field>> collect = declaredFields.stream().map(Field::getClass).collect(Collectors.toList());
-        Method[] declaredMethods = aClass.getDeclaredMethods();
-
-
-        PropertyDescriptor[] propertyDescriptors = Introspector.getBeanInfo(aClass, Object.class).getPropertyDescriptors();
+        PropertyDescriptor[] propertyDescriptors = Introspector.getBeanInfo(aClass, Object.class)
+                .getPropertyDescriptors();
 
         List<Method> getters = Arrays.stream(propertyDescriptors)
                 .map(PropertyDescriptor::getReadMethod)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()); //retrieving getters from parent object
 
-
-        List<Method> setters = Arrays.stream(propertyDescriptors)
-                .map(PropertyDescriptor::getWriteMethod)
-                .collect(Collectors.toList());
-
-        List<Object> parameters = getters.stream().map(getter -> {
+        List<Object> values = getters.stream().map(getter -> {
+            Object value = null;
             try {
-                return getter.invoke(object);
+                value = getter.invoke(parentObject);
             } catch (IllegalAccessException | InvocationTargetException e) {
                 e.printStackTrace();
             }
-            return null;
-        }).collect(Collectors.toList());
+            return value;
+        }).collect(Collectors.toList()); //retrieving values from parent object fields
 
-        List<? extends Class<?>> gettersReturnTypes = parameters.stream().map(Object::getClass).collect(Collectors.toList());
-
-        Class[] classes = new Class[gettersReturnTypes.size()];
-        gettersReturnTypes.toArray(classes);
+        List<Method> setters = Arrays.stream(propertyDescriptors)
+                .map(PropertyDescriptor::getWriteMethod)
+                .collect(Collectors.toList()); //retrieving setters from parent object
 
         Constructor<?> constructor = Arrays.stream(aClass.getDeclaredConstructors()).collect(Collectors.toList())
-                .stream().max(Comparator.comparing(Constructor::getParameterCount)).get();
+                .stream().min(Comparator.comparing(Constructor::getParameterCount)).get(); //retrieving constructor from parent object
+        //no need to check Optional on NutNULL cuz there will be at least one constructor in the class, for example default one
 
-        List<Class<?>> constructorParametersTypes = Arrays.stream(constructor.getParameterTypes()).collect(Collectors.toList());
+        List<Class<?>> constructorParametersTypes = Arrays.stream(constructor.getParameterTypes())
+                .collect(Collectors.toList()); //retrieving parameter types from constructor arguments
 
-        Object[] collect1 = constructorParametersTypes.stream()
+        Object[] defaultParameters = constructorParametersTypes.stream()
                 .map(o -> {
                     if (o.isPrimitive()){
-                        return 0; //check type and assign the value https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+                        switch (o.getTypeName())
+                        {
+                            case "byte":
+                            case "short":
+                            case "int":
+                                return 0;
+                            case "long":
+                                return 0L;
+                            case "float":
+                                return 0.0f;
+                            case "double":
+                                return 0.0d;
+                            case "char":
+                                return '\u0000';
+                            case "boolean":
+                                return false;
+                        }
                     }
                     return null;
                 })
-                .toArray();
+                .toArray(); //collecting an array of default values
 
-        return collect1;
-        //return constructorParametersTypes;
+        Object copiedObject;
+        if (constructorParametersTypes.size() != 0){
+            copiedObject = constructor.newInstance(defaultParameters);
+        } else {
+            copiedObject = constructor.newInstance();
+        } //filling new object by default values
+
+
+        Object finalCopiedObject = copiedObject;
+        setters.forEach(setter -> {
+            try {
+                setter.invoke(finalCopiedObject, values.get(setters.indexOf(setter)));
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }); //invoking setters for final object using values from parentObject
+
+        return finalCopiedObject;
     }
-
 }
--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,17 @@
 import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Test {
 
     public static void main(String[] args) throws NoSuchMethodException, IntrospectionException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        CopyMachine copyMachine = new CopyMachine();
 
         List<String> books = new ArrayList<>();
         books.add("book1");
@@ -13,15 +19,27 @@ public class Test {
         books.add("book3");
 
         Man man = new Man("name1", 99, books);
-        System.out.println(CopyMachine.deepCopy(man));
-//        Man man1 = new Man(null, (Integer)null, null);
-//        System.out.println(man1);
 
-        /*Object copiedMan = CopyMachine.deepCopy(man);
+        Object clonedObject = copyMachine.deepCopy(man);
+
+        PropertyDescriptor[] propertyDescriptors = Introspector.getBeanInfo(clonedObject.getClass(), Object.class)
+                .getPropertyDescriptors();
+
+        List<Object> fieldValuesOfClonedObject = Arrays.stream(propertyDescriptors)
+                .map(propertyDescriptor -> {
+                    try {
+                        return propertyDescriptor.getReadMethod().invoke(clonedObject);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                })
+                .collect(Collectors.toList());
 
         System.out.println(man);
-        System.out.println(copiedMan);
-        System.out.println(man == copiedMan);*/
+        System.out.println(clonedObject);
+        System.out.println(man.equals(clonedObject));
+        System.out.println(fieldValuesOfClonedObject);
     }
 
 }
